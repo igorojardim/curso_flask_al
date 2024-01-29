abbd8e0db58f4dbe36a05e21e04853b2ee6420f2
igorjardim <igorjardim@gmail.com>
2024-01-29 10:27:26 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+
+@app.route('/index')
+def index():
+    nome = "Igão!!!!!"
+    dados = {"profissao" : "Servidor Público", "orgao": "SES GO" }
+
+    return render_template('index.html', nome=nome, dados=dados)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
